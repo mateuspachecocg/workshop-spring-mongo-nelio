66511be01ce2b6f8f27f195d7cc0e80069ee6d2e
F7021234 Mateus Pacheco Uchoa <mateus.uchoa@bb.com.br>
2024-07-31 13:29:37 -0300

    Obtendo um post por id

---
+++ src/main/java/com/mateuspacheco/workshopmongo/resources/PostResource.java
@@ -0,0 +1,29 @@
+package com.mateuspacheco.workshopmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mateuspacheco.workshopmongo.domain.Post;
+import com.mateuspacheco.workshopmongo.services.PostService;
+
+/**
+ * PostResource
+ */
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+  @Autowired
+  private PostService postService;
+
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<Post> findById(@PathVariable String id) {
+    Post post = postService.findbyId(id);
+    return ResponseEntity.ok().body(post);
+  }
+
+}
---
+++ src/main/java/com/mateuspacheco/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.mateuspacheco.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mateuspacheco.workshopmongo.domain.Post;
+import com.mateuspacheco.workshopmongo.repository.PostRepository;
+import com.mateuspacheco.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+/**
+ * UserService
+ */
+@Service
+public class PostService {
+
+  @Autowired
+  private PostRepository postRepository;
+
+  public Post findbyId(String id) {
+    Optional<Post> obj = postRepository.findById(id);
+    return obj.orElseThrow(() -> new ObjectNotFoundException("Post nao encontrado!"));
+  }
+}
