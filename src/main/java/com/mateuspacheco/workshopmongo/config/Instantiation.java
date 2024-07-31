b123057c1965eb5807842237a35326c77981d12e
F7021234 Mateus Pacheco Uchoa <mateus.uchoa@bb.com.br>
2024-07-31 12:16:30 -0300

    Referenciando post do usuario

--- src/main/java/com/mateuspacheco/workshopmongo/config/Instantiation.java
+++ src/main/java/com/mateuspacheco/workshopmongo/config/Instantiation.java
@@ -45,5 +45,10 @@ public class Instantiation implements CommandLineRunner {
     Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
     postRepository.saveAll(Arrays.asList(post1, post2));
+
+    maria.addPost(post1);
+    maria.addPost(post2);
+
+    userRepository.save(maria);
   }
 }
