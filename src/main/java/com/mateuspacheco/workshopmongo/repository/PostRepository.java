d5db4e88f15e3d59d22eded46ebd6097ae63a474
F7021234 Mateus Pacheco Uchoa <mateus.uchoa@bb.com.br>
2024-08-01 10:47:25 -0300

    Consulta com varios criterios

--- src/main/java/com/mateuspacheco/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/mateuspacheco/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.mateuspacheco.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -18,5 +19,8 @@ public interface PostRepository extends MongoRepository<Post, String> {
   List<Post> searchTitle(String text);
 
   List<Post> findByTitleContaining(String text);
+
+  @Query("{ $and: [ { 'date': {$gte: ?1}}, { 'date': {$lte: ?2} } , { $or: [ {'title': {$regex: ?0, $options: 'i'}},  {'body': {$regex: ?0, $options: 'i'}}, {'comments.text': {$regex: ?0, $options: 'i'}}] } ] } ")
+  List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 }
